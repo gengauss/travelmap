bd31d85178ca473b3a2974dbc82633a5f8133b46
Nguyen Quynh Anh <s1f101800070@iniad.org>
2019-05-31 11:23:32 +0900

    addmap

--- public/src/app.js
+++ public/src/app.js
@@ -3,5 +3,5 @@ var app = angular.module('myApp', []);
 app.controller('myController', myController);
 
 function myController($scope, $http) {
-  $scope.title = 'Hello World';
+  $scope.title = 'Hello';
 }
